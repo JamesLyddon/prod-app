4d3f298754b0e6f0ce0a36cf23f8b56136652fdc
James Lyddon <jameslyddon@gmail.com>
2022-04-07 17:49:18 +0100

    refactored timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 
 export default function Timer() {
-  const [timeLeft, setTimeLeft] = useState(5);
-  const [isActive, setIsActive] = useState(false);
+  const [timer, setTimer] = useState(10000);
+  const [isRunning, setIsRunning] = useState(false);
 
-  function countDown() {
-    const timer = setInterval(() => {
-      setTimeLeft((timeLeft) => timeLeft - 1);
-    }, 1000);
-  }
+  useEffect(() => {
+    let interval = null;
+
+    // time incrementer
+    if (isRunning) {
+      interval = setInterval(() => {
+        setTimer((prevTime) => prevTime - 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+
+    // cleanup function
+    return () => clearInterval(interval);
+  }, [isRunning]);
 
   return (
     <Container>
-      <Button onClick={countDown}>click</Button>
-      <p>{timeLeft}</p>
+      <Button onClick={() => setIsRunning(true)}>Start</Button>
+      <Button onClick={() => setIsRunning(false)}>Stop</Button>
+      <Button onClick={() => setIsRunning(true)}>Resume</Button>
+      <Button onClick={() => setTimer(0)}>Clear</Button>
+      <p>{timer}</p>
     </Container>
   );
 }
