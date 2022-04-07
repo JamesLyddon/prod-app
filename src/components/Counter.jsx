dbe35a46a68d53c345c0be05b750d90ef5492b17
James Lyddon <jameslyddon@gmail.com>
2022-04-07 13:12:34 +0100

    finished counter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,27 +1,62 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Container, Col, Row, Button, Alert, Form } from "react-bootstrap";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+
   return (
-    <div className="container">
-      <div>
-        <span>Counter</span>
-      </div>
-      <div>
-        <Button className="btn btn-warning" onClick={() => setCount(count - 1)}>
-          -
-        </Button>
-        <span className="fs-4">{count}</span>
-        <Button className="btn btn-primary" onClick={() => setCount(count + 1)}>
-          +
-        </Button>
-      </div>
-      <div>
-        <Button className="btn btn-danger" onClick={() => setCount(0)}>
-          reset
-        </Button>
-      </div>
-    </div>
+    <Container className="text-center">
+      <Row>
+        <Col lg={{ span: 3 }}>
+          <Alert variant="info" className="m-4">
+            <Form>
+              <Form.Control
+                className="text-center"
+                type="text"
+                placeholder="name me..."
+              ></Form.Control>
+            </Form>
+          </Alert>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={{ span: 3 }} className="d-flex justify-content-around">
+          <Button
+            className="btn btn-warning"
+            onClick={() => setCount(count - 1)}
+          >
+            -
+          </Button>
+          <span className="fs-4">{count}</span>
+          <Button
+            className="btn btn-primary"
+            onClick={() => setCount(count + 1)}
+          >
+            +
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={{ span: 3 }}>
+          <Alert
+            variant="warning"
+            className="m-4 d-flex justify-content-around"
+          >
+            <Button className="btn btn-warning p-1" onClick={() => setCount(0)}>
+              save
+            </Button>
+            <Button className="btn btn-warning p-1" onClick={() => setCount(0)}>
+              load
+            </Button>
+            <Button
+              className="btn btn-warning p-1 "
+              onClick={() => setCount(0)}
+            >
+              reset
+            </Button>
+          </Alert>
+        </Col>
+      </Row>
+    </Container>
   );
 }
