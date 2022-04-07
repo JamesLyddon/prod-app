6ab57251054b11fb2c111dc5e889cc48de240e62
James Lyddon <jameslyddon@gmail.com>
2022-04-07 21:09:32 +0100

    set up spinner

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -32,11 +32,25 @@ export default function Timer() {
 
   return (
     <Container>
+      {isRunning ? (
+        <Row>
+          <Col lg={{ span: 3 }}>
+            <div className="d-flex justify-content-center align-items-center text-center mb-4">
+              <span className="position-absolute text-primary font-weight-bolder">
+                {timer}
+              </span>
+              <span className="spinner-border text-warning lh-1" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </span>
+            </div>
+          </Col>
+        </Row>
+      ) : null}
+
       <Button onClick={() => setIsRunning(true)}>Start</Button>
       <Button onClick={() => setIsRunning(false)}>Stop</Button>
       <Button onClick={() => setIsRunning(true)}>Resume</Button>
       <Button onClick={() => setTimer(0)}>Clear</Button>
-      <p>{timer}</p>
       {timerComplete ? <p>Finished!</p> : ""}
     </Container>
   );
